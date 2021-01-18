e7e7bd20aa7e73b7c0491c0fdc0dae99def3fe10
Motasem odeh <motasem.udeh@gmail.com>
2021-01-18 12:59:46 +0200

    final

--- app.js
+++ app.js
@@ -3,10 +3,11 @@
 var imageOneElement = document.getElementById('image-one');
 var imageTwoElement = document.getElementById('image-two');
 var imageThreeElement = document.getElementById('image-three');
+var buttonElement = document.getElementById('show-result-bitton');
+var imagesHolderElement = document.getElementById('imageVotes');
+var numberOfRoundsForm = document.getElementById('numberofrounds');
 var maxAttempts = 25;
 var userAttemptsCounter = 0;
-// var imageAppearence = 0;
-
 var imageOneIndex;
 var imageTwoIndex;
 var imageThreeIndex;
@@ -17,8 +18,9 @@ function ImageMall(imageName, imageSource)
     this.imageName = imageName;
     this.imageSource = imageSource;
     this.timesImagesHasBeenShown = 0;
+    this.imageAppearence = 0;
     ImageMall.prototype.allImages.push(this);
-    
+
 }
 
 ImageMall.prototype.allImages = [];
@@ -28,50 +30,54 @@ new ImageMall('banana','img/banana.jpg');
 new ImageMall('boots','img/boots.jpg');
 new ImageMall('bathroom','img/bathroom.jpg');
 new ImageMall('breakfast','img/breakfast.jpg');
-
 new ImageMall('bubblegum','img/bubblegum.jpg');
 new ImageMall('chair','img/chair.jpg');
 new ImageMall('cthulhu','img/cthulhu.jpg');
 new ImageMall('dog duck','img/dog-duck.jpg');
 new ImageMall('dragon','img/dragon.jpg');
 new ImageMall('pen','img/pen.jpg');
-
 new ImageMall('pet sweep','img/pet-sweep.jpg');
 new ImageMall('scissors','img/scissors.jpg');
 new ImageMall('Shark','img/shark.jpg');
 new ImageMall('tauntaun','img/tauntaun.jpg');
 new ImageMall('Unicorn','img/unicorn.jpg');
 new ImageMall('usb','img/usb.gif');
-
 new ImageMall('Water can','img/water-can.jpg');
 new ImageMall('Wine Glass','img/wine-glass.jpg');
 
 renderThreeRandomImages();
 
-imageOneElement.addEventListener('click',handleUserClick);
-imageTwoElement.addEventListener('click',handleUserClick);
-imageThreeElement.addEventListener('click',handleUserClick);
+imagesHolderElement.addEventListener('click',handleUserClick)
+buttonElement.addEventListener('click',showResult);
+numberOfRoundsForm.addEventListener('submit',userDefinedroundNumbers);
 
 function handleUserClick(event){
   userAttemptsCounter++;
 
-  if(userAttemptsCounter <= maxAttempts){
+  if(userAttemptsCounter < maxAttempts){
     if(event.target.id === 'image-one'){
         ImageMall.prototype.allImages[imageOneIndex].timesImagesHasBeenShown++;
+        renderThreeRandomImages();
+
     } else if (event.target.id === 'image-two') {
         ImageMall.prototype.allImages[imageTwoIndex].timesImagesHasBeenShown++;
-    }  else {
+        renderThreeRandomImages();
+    }  else if (event.target.id === 'image-three'){
         ImageMall.prototype.allImages[imageThreeIndex].timesImagesHasBeenShown++;
+        renderThreeRandomImages();
     }
-    renderThreeRandomImages();
 
   } else {
-    
-    showResult();
-    
+
+    imagesHolderElement.removeEventListener('click',handleUserClick);
+
+    buttonElement.disabled=false;
+
   }
 
 }
+
+
 var resultsList;
 var finalResult;
 function showResult()
@@ -79,56 +85,44 @@ function showResult()
       // handle end of voting
       resultsList = document.getElementById('results-list');
       finalResult;
-
       
   for(var i = 0; i < ImageMall.prototype.allImages.length; i++){
 
     finalResult = document.createElement('li');
-    finalResult.textContent = ImageMall.prototype.allImages[i].imageName + ' had  '+ ImageMall.prototype.allImages[i].timesImagesHasBeenShown + ' Votes, and was seen ' +  + ' Times';
+    finalResult.textContent = ImageMall.prototype.allImages[i].imageName + ' had  '+ ImageMall.prototype.allImages[i].timesImagesHasBeenShown + ' Votes, and was seen ' + ImageMall.prototype.allImages[i].imageAppearence + ' Times' + 'and its Persentage was ' + (ImageMall.prototype.allImages[i].timesImagesHasBeenShown * 100 / ImageMall.prototype.allImages[i].imageAppearence) + '%';
     
     resultsList.appendChild(finalResult);
-    
+   
   }
-
-      imageOneElement.removeEventListener('click',handleUserClick);
-      imageTwoElement.removeEventListener('click',handleUserClick);
-      imageThreeElement.removeEventListener('click',handleUserClick);
-  
+  buttonElement.removeEventListener('click',showResult);
 }
-
-
-function showResultButton()
-{
-  
-  var btn = document.createElement("BUTTON"); 
-  btn.id ='myBtn';
-
-}
-
 
 function renderThreeRandomImages(){
-    // imageOneIndex = generateRandomIndex();
-    // imageTwoIndex = generateRandomIndex();
+  imageOneIndex = generateRandomIndex();
   
     do{
-        imageOneIndex = generateRandomIndex();
+
         imageTwoIndex = generateRandomIndex();
         imageThreeIndex = generateRandomIndex();
     } while(imageOneIndex === imageTwoIndex || imageTwoIndex === imageThreeIndex || imageOneIndex === imageThreeIndex );
     imageOneElement.src = ImageMall.prototype.allImages[imageOneIndex].imageSource;
+    ImageMall.prototype.allImages[imageOneIndex].imageAppearence++;
     imageTwoElement.src = ImageMall.prototype.allImages[imageTwoIndex].imageSource;
+    ImageMall.prototype.allImages[imageTwoIndex].imageAppearence++;
     imageThreeElement.src = ImageMall.prototype.allImages[imageThreeIndex].imageSource;
+    ImageMall.prototype.allImages[imageThreeIndex].imageAppearence++;
     
     console.log(imageOneIndex, imageTwoIndex, imageThreeIndex)
     console.log(imageOneIndex)
 
   }
 
-//   function renderNewImages(){
-   
-//   }
-  
   function generateRandomIndex(){
     return Math.floor(Math.random() * (ImageMall.prototype.allImages.length));
   }
   
+  function userDefinedroundNumbers(event)
+{
+  event.preventDefault();
+  maxAttempts = event.target.rounds.value;
+}
